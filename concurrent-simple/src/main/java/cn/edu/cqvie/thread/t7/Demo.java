d444461f6f6ef0b3ec9594094dcc6ad01221c6a5
382333604@qq.com <xxx@xxx.com>
2020-02-19 20:14:13 +0800

    simu dead lock

---
+++ concurrent-simple/src/main/java/cn/edu/cqvie/thread/t7/Demo.java
@@ -0,0 +1,43 @@
+package cn.edu.cqvie.thread.t7;
+
+import lombok.SneakyThrows;
+
+/**
+ * 保证可见性的前提
+ * 多个线程拿到的是同一把锁
+ *
+ * @author zhengsh
+ * @date 2020-02-18
+ */
+public class Demo {
+
+    private volatile int a = 1;
+
+    public synchronized int getA() {
+        return a;
+    }
+
+    @SneakyThrows
+    public synchronized void setA(int a) {
+        Thread.sleep(10);
+        this.a = a;
+    }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+
+        Demo demo = new Demo();
+
+        new Thread(() -> {
+            demo.setA(10);
+        }).start();
+
+        new Thread(() -> {
+            System.out.println(demo.getA());
+        }).start();
+
+        Thread.sleep(100);
+        System.out.println(demo.getA());
+
+    }
+}
---
+++ concurrent-simple/src/main/java/cn/edu/cqvie/thread/t8/User.java
@@ -0,0 +1,7 @@
+package cn.edu.cqvie.thread.t8;
+
+public class User {
+
+    public volatile Integer id;
+    public volatile String name;
+}
